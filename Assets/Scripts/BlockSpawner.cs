b56c8d5211439c16292b56daf78a5c39e9793a49
Yagi2001 <ramazanbatu2001@outlook.com>
2025-01-09 01:23:38 +0300

    CompletedBlockSpawner
    Added new featuers like rotation of block

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 
 public class BlockSpawner : MonoBehaviour
 {
-    [SerializeField]
-    private Transform _spawnPoint;
+    private List<GameObject> _availableCubes;
     [SerializeField]
     private GameObject[] _cubePrefabs;
     [SerializeField]
@@ -12,32 +11,44 @@ public class BlockSpawner : MonoBehaviour
 
     private void Start()
     {
+        _availableCubes = new List<GameObject>( _cubePrefabs );
         SpawnRandomBlock();
     }
 
     private void SpawnRandomBlock()
     {
+        ResetAvailableCubes();
         int randomIndex = Random.Range( 0, _configurationPrefabs.Length );
-        GameObject newBlock = Instantiate( _configurationPrefabs[randomIndex], _spawnPoint.position, Quaternion.identity );
+        GameObject newBlock = Instantiate( _configurationPrefabs[randomIndex], transform.position, Quaternion.identity );
         List<GameObject> usedCubes = new List<GameObject>();
 
         foreach (Transform part in newBlock.transform)
         {
             GameObject randomCube = GetUniqueCubePrefab( usedCubes );
-            GameObject newCube = Instantiate( randomCube, part.position, part.rotation );
+            GameObject newCube = Instantiate( randomCube, part.position, part.rotation, newBlock.transform );
             newCube.transform.localScale = part.localScale;
             Destroy( part.gameObject );
         }
+        newBlock.transform.rotation = Quaternion.Euler( 0, 0, GetRandomRotation() );
     }
 
+    // Ensures that the same color is not used more than once on the block
     private GameObject GetUniqueCubePrefab( List<GameObject> usedCubes )
     {
-        GameObject randomCube;
-        do
-        {
-            randomCube = _cubePrefabs[Random.Range( 0, _cubePrefabs.Length )];
-        } while (usedCubes.Contains( randomCube ));
-        usedCubes.Add( randomCube );
-        return randomCube;
+        int randomIndex = Random.Range( 0, _availableCubes.Count );
+        GameObject selectedCube = _availableCubes[randomIndex];
+        _availableCubes.RemoveAt( randomIndex );
+        return selectedCube;
+    }
+
+    private float GetRandomRotation()
+    {
+        int randomAngle = Random.Range( 1, 5 ) * 90;
+        return randomAngle;
+    }
+
+    private void ResetAvailableCubes()
+    {
+        _availableCubes = new List<GameObject>( _cubePrefabs );
     }
 }
