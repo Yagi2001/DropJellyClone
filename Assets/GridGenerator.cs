d83306cc1ce46a1d9c4788a822b95328f31f192d
Yagi2001 <ramazanbatu2001@outlook.com>
2025-01-08 22:48:13 +0300

    AddedGrids

---
+++ Assets/GridGenerator.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private int _rows = 6;
+    [SerializeField]
+    private int _columns = 6;
+    [SerializeField]
+    private float _spacing = 0f;
+    [SerializeField]
+    private Transform _parentGrid;
+    [SerializeField]
+    private GameObject _tilePrefab;
+
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    void GenerateGrid()
+    {
+        for (int row = 0; row < _rows; row++)
+        {
+            for (int col = 0; col < _columns; col++)
+            {
+                float xPosition = col + col * _spacing;
+                float yPosition = row + row * _spacing;
+                Vector3 position = new Vector3( xPosition, yPosition, 0 );
+                Instantiate( _tilePrefab, position, Quaternion.identity, _parentGrid );
+            }
+        }
+    }
+}
